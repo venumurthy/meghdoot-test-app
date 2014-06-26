05b92821b1a922fa4a949631c1fc8d56121253a2
Asif R Choudhury <choudhury.asif@gmail.com>
2014-06-26 16:58:34 -0500

    Asif | Removed failing test

--- test/passingTest.js
+++ test/passingTest.js
@@ -5,10 +5,4 @@ describe("Passing Test", function() {
     var test = true;
     test.should.be.ok;
   });
-  describe("Failing Test", function() {
-    it ("should fail", function() {
-      var test = false;
-      test.should.be.ok;
-    });
-  });
 });
