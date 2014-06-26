b2c84a4a12bab935e0357746350372ce3d4a8dd1
Asif R Choudhury <choudhury.asif@gmail.com>
2014-06-26 16:55:55 -0500

    Asif | Added failing test

--- test/passingTest.js
+++ test/passingTest.js
@@ -5,4 +5,10 @@ describe("Passing Test", function() {
     var test = true;
     test.should.be.ok;
   });
+  describe("Failing Test", function() {
+    it ("should fail", function() {
+      var test = false;
+      test.should.be.ok;
+    });
+  });
 });
