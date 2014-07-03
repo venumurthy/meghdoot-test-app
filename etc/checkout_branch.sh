7eb325bc48c1f78529b2aa5012b08793ee0fcba5
Asif R Choudhury <choudhury.asif@gmail.com>
2014-07-03 00:47:16 -0500

    Asif | Shell scripts to switch branches and pull latest for each openstack component

--- etc/checkout_branch.sh
+++ etc/checkout_branch.sh
@@ -1,22 +1,29 @@
+#!/bin/sh
+
+#cinder
+#glance
+#heat
+#horizon
+#keystone
+#nova
+
 export BRANCH=master
 
+echo Checking out $BRANCH in cinder...
 cd /opt/stack/cinder
 git checkout $BRANCH
+echo Checking out $BRANCH in glance...
 cd /opt/stack/glance
 git checkout $BRANCH
+echo Checking out $BRANCH in heat...
 cd /opt/stack/heat
 git checkout $BRANCH
+echo Checking out $BRANCH in horizon...
 cd /opt/stack/horizon
 git checkout $BRANCH
+echo Checking out $BRANCH in keystone...
 cd /opt/stack/keystone
 git checkout $BRANCH
+echo Checking out $BRANCH in nova...
 cd /opt/stack/nova
 git checkout $BRANCH
-
-
-#cd /opt/stack/cinder
-#cd /opt/stack/glance
-#cd /opt/stack/heat
-#cd /opt/stack/horizon
-#cd /opt/stack/keystone
-#cd /opt/stack/nova
--- etc/update_branch.sh
+++ etc/update_branch.sh
@@ -1,12 +1,20 @@
+#!/bin/sh
+
+echo Checking out $BRANCH in cinder...
 cd /opt/stack/cinder
 git pull
+echo Checking out $BRANCH in glance...
 cd /opt/stack/glance
 git pull
+echo Checking out $BRANCH in heat...
 cd /opt/stack/heat
 git pull
+echo Checking out $BRANCH in horizon...
 cd /opt/stack/horizon
 git pull
+echo Checking out $BRANCH in keystone...
 cd /opt/stack/keystone
 git pull
+echo Checking out $BRANCH in nova...
 cd /opt/stack/nova
 git pull
